1892e2f6b43c1168211bc3a4a50e78e88c024a62
ShaqEd92 <shaqhaedwards@gmail.com>
2019-10-18 20:39:30 -0500

    Deleted unnecessary components, front end - main page, logo

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { homeComponent } from './home/home.component';
 import { playComponent } from './play/play.component';
 import { statsComponent } from './stats/stats.component';
 import { boardComponent } from './board/board.component';
-import { errorComponent } from './error/error.component';
 
 const routes: Routes = [
-  { path: 'home', component: homeComponent },
   { path: 'play', component: playComponent },
   { path: 'stats', component: statsComponent },
   { path: 'board', component: boardComponent },
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
-  { path: '**', component: errorComponent }
 ];
 
 @NgModule({
